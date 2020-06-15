564ac423a4809a3d3a08a60da607e21c8b42ae07
Nikita Denysenko <nik2012nik92@gmail.com>
2020-06-16 00:01:58 +0300

    Added Routing and Orders Functionality

--- burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,28 +87,16 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Nick',
-                address: {
-                    street: 'TestStreet 1',
-                    zipCode: '12314',
-                    country: 'Ukraine'
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(res => {
-                this.setState({loading: false, purchasing:false});
-            })
-            .catch(err => {
-                this.setState({loading: false, purchasing:false});
-            });
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push ({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
     };
 
     render() {
